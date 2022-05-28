238962d5ece5ae1525e2e71156cf22352c53e8f6
Filip <fjovanovic10020rn@raf.rs>
2022-05-28 20:43:44 +0200

    Zapravo prvi komit

---
+++ src/main/java/resource/implementation/Entity.java
@@ -0,0 +1,24 @@
+package resource.implementation;
+
+import lombok.Getter;
+import lombok.Setter;
+import resource.DBNode;
+import resource.DBNodeComposite;
+
+@Getter
+@Setter
+public class Entity extends DBNodeComposite {
+
+    public Entity(String name, DBNode parent) {
+        super(name, parent);
+    }
+
+    @Override
+    public void addChild(DBNode child) {
+        if (child != null && child instanceof Attribute){
+            Attribute attribute = (Attribute) child;
+            this.getChildren().add(attribute);
+        }
+
+    }
+}
